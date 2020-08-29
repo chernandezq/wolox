f4c45f8189f78c0f70b89c769bab708245f630cf
Cristian Hernandez <ing.cristianh@gmail.com>
2020-08-29 16:16:33 -0500

    vista others

---
+++ src/screens/others/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Other = () => {
+  return (
+    <View style={{flex: 1, marginLeft: 20, marginRight: 20}}>
+      <Text>Otra vista</Text>
+    </View>
+  );
+};
+
+export default Other;
