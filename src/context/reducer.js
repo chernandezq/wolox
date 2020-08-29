ff1d12c03f4be9479e7a3c68d59c7ed16fc55c31
Cristian Hernandez <ing.cristianh@gmail.com>
2020-08-29 10:41:19 -0500

    reducer de context api para manejo de estados globales y funciones

---
+++ src/context/reducer.js
@@ -0,0 +1,41 @@
+import AsyncStorage from '@react-native-community/async-storage';
+
+export const reducer = (state, action) => {
+  switch (action.type) {
+    case 'LOGIN':
+      const isAutenticated = ['@isAutenticated', '1'];
+      const access_token = [
+        '@access_token',
+        JSON.stringify(action.payload.access_token),
+      ];
+      const token_type = [
+        '@token_type',
+        JSON.stringify(action.payload.token_type),
+      ];
+      AsyncStorage.multiSet([access_token, token_type, isAutenticated]);
+      return {
+        ...state,
+        isAutenticated: true,
+        access_token: action.payload.access_token,
+        token_type: action.payload.token_type,
+      };
+    case 'LOGOUT':
+      AsyncStorage.clear();
+      return {
+        ...state,
+        isAutenticated: false,
+        access_token: null,
+        token_type: null,
+        refresh_token: null,
+      };
+    case 'GET_STORAGE':
+      return {
+        ...state,
+        isAutenticated: true,
+        access_token: action.payload.access_token,
+        token_type: action.payload.token_type,
+      };
+    default:
+      return state;
+  }
+};
